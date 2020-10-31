97cdbd17a75899b8ed93f8de7b5f87bc54372def
silverawz <deroussenicolas@gmail.com>
2020-10-31 20:53:37 +0100

    new algo

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,32 @@
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+        //This is unsorted array
+        Integer[] array = new Integer[] {12,13,24,10,3,6,90,70};
+         
+        //Let's sort using insertion sort
+        insertionSort(array, 0, array.length);
+         
+        //Verify sorted array
+        System.out.println(Arrays.toString(array));
 	}
 
-	public class Quicksort {
-	    private int[] numbers;
-	    private int number;
-
-	    public void sort(int[] values) {
-	        this.numbers = values;
-	        number = values.length;
-	        quicksort(0, number - 1);
-	    }
-
-	    private void quicksort(int low, int high) {
-	        int i = low, j = high;
-	        // Get the pivot element from the middle of the list
-	        int pivot = numbers[(low + high) / 2];
-
-	        // Divide into two lists
-	        while (i <= j) {
-	            // If the current value from the left list is smaller then the pivot
-	            // element then get the next element from the left list
-	            while (numbers[i] < pivot) {
-	                i++;
-	            }
-	            // If the current value from the right list is larger then the pivot
-	            // element then get the next element from the right list
-	            while (numbers[j] > pivot) {
+	  @SuppressWarnings({ "rawtypes", "unchecked" })
+	    public static void insertionSort(Object[] a, int fromIndex, int toIndex) 
+	    {
+	        Object d;
+	        for (int i = fromIndex + 1; i < toIndex; i++) 
+	        {
+	            d = a[i];
+	            int j = i;
+	            while (j > fromIndex && ((Comparable) a[j - 1]).compareTo(d) > 0) 
+	            {
+	                a[j] = a[j - 1];
 	                j--;
 	            }
-
-	            // If we have found a values in the left list which is larger then
-	            // the pivot element and if we have found a value in the right list
-	            // which is smaller then the pivot elment then we exchange the
-	            // values.
-	            if (i <= j) {
-	                exchange(i, j);
-	                i++;
-	                j--;
-	            }
+	            a[j] = d;
 	        }
-	        // Recursion
-	        if (low < j)
-	            quicksort(low, j);
-	        if (i < high)
-	            quicksort(i, high);
 	    }
-
-	    private void exchange(int i, int j) {
-	        int temp = numbers[i];
-	        numbers[i] = numbers[j];
-	        numbers[j] = temp;
-	    }
-	}
-	
 }
