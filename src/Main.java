c88f9038abde048e7b8856337ffde8ec0096fe87
silverawz <deroussenicolas@gmail.com>
2020-11-06 14:20:38 +0100

    tri par selection

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,42 @@ import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) {
-		int array[] = {3,6,9,4,2,1};
-		rotate(array, 3);
-		for (int i : array) {
-			System.out.println(i);
-		}
-	}
-
-	public static void rotate(int[] nums, int k) {
-	    if(k > nums.length) 
-	        k=k%nums.length;
-	 
-	    int[] result = new int[nums.length];
-	 
-	    for(int i=0; i < k; i++){
-	        result[i] = nums[nums.length-k+i];
-	    }
-	 
-	    int j=0;
-	    for(int i=k; i<nums.length; i++){
-	        result[i] = nums[j];
-	        j++;
-	    }
-	 
-	    System.arraycopy( result, 0, nums, 0, nums.length );
-	}
+	 public static void tri_selection(int[] tab)
+     {
+          for (int i = 0; i < tab.length - 1; i++)  
+          {
+               int index = i;  
+               for (int j = i + 1; j < tab.length; j++)
+               {
+                    if (tab[j] < tab[index]){ 
+                         index = j;
+                    }
+               }
+ 
+               int min = tab[index];
+               tab[index] = tab[i]; 
+               tab[i] = min;
+          }
+     }
+     static void displayTab(int[] tab){
+          for(int i=0; i < tab.length; i++)
+          {  
+               System.out.print(tab[i] + " ");  
+          } 
+          System.out.println();    
+     }
+  
+     public static void main(String a[])
+     {  
+          int[] tab = {-4, 1, 13, 4, 16, 2, 13, 14}; 
+  
+          System.out.println("**** Avant le tri par selection *****");
+          displayTab(tab);
+          //tri d un tableau avec l algorithme de tri par selection
+          tri_selection(tab);
+ 
+          System.out.println("**** Apres le tri par selection ****");  
+          displayTab(tab); 
+     } 
 
 }
