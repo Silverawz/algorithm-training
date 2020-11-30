2f151ffc25a6998db619c77d3272e6c4fa029eb3
silverawz <deroussenicolas@gmail.com>
2020-11-30 19:29:20 +0100

    algo codewars.com added

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,34 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
 
-	 public static void tri_selection(int[] tab)
-     {
-          for (int i = 0; i < tab.length - 1; i++)  
-          {
-               int index = i;  
-               for (int j = i + 1; j < tab.length; j++)
-               {
-                    if (tab[j] < tab[index]){ 
-                         index = j;
-                    }
-               }
- 
-               int min = tab[index];
-               tab[index] = tab[i]; 
-               tab[i] = min;
-          }
-     }
-     static void displayTab(int[] tab){
-          for(int i=0; i < tab.length; i++)
-          {  
-               System.out.print(tab[i] + " ");  
-          } 
-          System.out.println();    
-     }
-  
-     public static void main(String a[])
-     {  
-          int[] tab = {-4, 1, 13, 4, 16, 2, 13, 14}; 
-          System.out.println("**** Avant le tri par selection *****");
-          displayTab(tab);
-          //tri d un tableau avec l algorithme de tri par selection
-          tri_selection(tab);
-          System.out.println("**** Apres le tri par selection ****");  
-          displayTab(tab); 
-     } 
 
+	public static void main(String args[]) {
+		String[]result = solution("abcdefghi");
+		for (String string : result) {
+			System.out.println(string);
+		}
+	}
+
+	 public static String[] solution(String s) {	
+		 int length = s.length();	
+		 String[] result = new String[length/2];
+		 if(length%2 != 0) {
+			 result = new String[(length/2+1)];
+		 }	
+		 int lengthResult = result.length;
+		 int k = 0;
+		 for(int i = 0; i < lengthResult; i++) {
+			 if(k+1 < length) result[i] = ""+s.charAt(k)+s.charAt(k+1)+"";
+			 else result[i] = ""+s.charAt(k)+"";
+			 k += 2;
+		 }		 
+		 if(length%2 != 0) {			 
+			 result[result.length-1] += "_";
+		 }
+		 return result;
+	 }
 }
