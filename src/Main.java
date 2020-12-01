b9e046b5182173b390ea045a496db6be2cc54635
silverawz <deroussenicolas@gmail.com>
2020-12-01 17:01:35 +0100

    training algo, one still not finished

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,55 @@
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+
 public class Main {
 
 	public static void main(String args[]) {
+		//System.out.println(validatePin("61.2"));
+		String[] result  = wave("two words");
+		for (String string : result) {
+			System.out.println(string);
+		}
 
 		//System.out.println(isValid("([{}])"));
-
-		System.out.println(isValid("({})[({})]"));
-
+		//System.out.println(isValid("({})[({})]"));
 	}
 
+
+	
+	public static String[] wave(String str) {
+		int charNumber = 0;
+		for(int i = 0; i < str.length();i++) {
+			if(!(str.charAt(i) == ' ') && !(Character.isDigit(str.charAt(i)))) {
+				charNumber++;
+			}	
+		}
+		String[] result = new String[charNumber];
+		int index = 0;
+		for(int i = 0; i < str.length();i++) {
+			if(!(str.charAt(i) == ' ') && !(Character.isDigit(str.charAt(i)))) {		
+				char[] arr = str.toCharArray();
+				arr[i] = Character.toUpperCase(arr[i]);
+				String newString = new String(arr);
+				result[index] = newString;
+				index++;
+			}	
+		}
+        return result;
+    }
+    
+	/*
+
+	public static boolean validatePin(String pin) {	
+		return pin.matches("\\d{4}|\\d{6}");
+	  }
+	  
+	  
+	  
+	  
 	public static boolean isValid(String braces) {
 		System.out.println("Input value: \"" + braces + "\"");
 		boolean result = false;
@@ -99,6 +136,6 @@ public class Main {
 		return s.charAt(index);
 	}
 	
-	
+*/	
 	
 }
